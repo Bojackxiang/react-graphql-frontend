5fd6b9bb1becc4da0aa1f8def1f89c264faf2409
alex <batmandocode@gmail.com>
2020-03-11 09:55:11 +1100

    solve the cros problem

--- src/Pages/auth.js
+++ src/Pages/auth.js
@@ -1,48 +1,47 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Auth = () => {
-    const [form, setForm] = useState({email: '', password: ''}); 
-    const formSubmit = async (event) => {
-        event.preventDefault()
-        const requestBody = {
-            query: `
-                createUser(userInput: {email: "useremail", password: "111111"}){
+  const [form, setForm] = useState({ email: "", password: "" });
+  const formSubmit = async event => {
+    event.preventDefault();
+    const requestBody = {
+      query: `
+                mutation{createUser(userInput: {email: "useremail", password: "111111"}){
                     email
                     password
-                }
-            `,
-        }
+                }}
+            `
+    };
 
-        try {
-            fetch('http://localhost:3005/graphql', {
-                method: 'POST',
-                body: JSON.stringify(requestBody), 
-                headers: {
-                    'Content-Type':'application/json'
-                }
-            }).then(resp => {console.log(resp)})
-            
-        } catch (error) {
-            
-        }
-        
-    }
+    axios
+      .post("http://localhost:8080/graphql", { ...requestBody })
+      .then(resp => {
+        console.log(resp);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
 
-    const inputChange = (event) => {
-        setForm({...form, [event.target.name]: event.target.value})
-    }
-    return (
-        <div>
-            <p>this is the auth page</p>
-            <form onSubmit={formSubmit}>
-                <input placeholder="email input" name="email" onChange={inputChange}/>
-                <input placeholder="password input" name="password" onChange={inputChange}/>
-                <button type="submit">submit</button>
-            </form>
-            
-            
-        </div>
-    )
-}
+
+  const inputChange = event => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+  return (
+    <div>
+      <p>this is the auth page</p>
+      <form onSubmit={formSubmit}>
+        <input placeholder="email input" name="email" onChange={inputChange} />
+        <input
+          placeholder="password input"
+          name="password"
+          onChange={inputChange}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Auth;
