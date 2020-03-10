ef286910df33967957ae17496a1b45d1a395d737
alex <batmandocode@gmail.com>
2020-03-10 19:01:31 +1100

    day3 update

--- src/Pages/auth.js
+++ src/Pages/auth.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Auth = () => {
+    const [form, setForm] = useState({email: '', password: ''}); 
+    const formSubmit = async (event) => {
+        event.preventDefault()
+        const requestBody = {
+            query: `
+                createUser(userInput: {email: "useremail", password: "111111"}){
+                    email
+                    password
+                }
+            `,
+        }
+
+        try {
+            fetch('http://localhost:3005/graphql', {
+                method: 'POST',
+                body: JSON.stringify(requestBody), 
+                headers: {
+                    'Content-Type':'application/json'
+                }
+            }).then(resp => {console.log(resp)})
+            
+        } catch (error) {
+            
+        }
+        
+    }
+
+    const inputChange = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
     return (
         <div>
-            auth page
+            <p>this is the auth page</p>
+            <form onSubmit={formSubmit}>
+                <input placeholder="email input" name="email" onChange={inputChange}/>
+                <input placeholder="password input" name="password" onChange={inputChange}/>
+                <button type="submit">submit</button>
+            </form>
+            
+            
         </div>
     )
 }
